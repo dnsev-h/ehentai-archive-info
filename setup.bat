ca27df80791ba656930356b950e40c0c88300827
dnsev-h <dnsev-h@users.noreply.github.com>
2019-05-29 21:02:17 -0400

    Update setup to print status info

--- setup.bat
+++ setup.bat
@@ -1,4 +1,18 @@
 @echo off
 pushd "%~dp0"
-npm install
+call npm install || goto :error
 popd
+color a
+echo Setup successful!
+pause
+color
+goto :eof
+
+:error
+color c
+echo.
+echo npm failed to run
+echo Install node and npm from https://nodejs.org/
+pause
+color
+goto :eof
